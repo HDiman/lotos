3ff520a280b5197f0a91ca480660275046c434e0
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-09 21:53:59 +0300

    Choose color

--- my_lotos/main/views.py
+++ my_lotos/main/views.py
@@ -17,7 +17,14 @@ def index(request):
         'item5_8': 'Достависта',
 
         'item3_0': 'Профи',
-        'item3_1': 'Поиск',
+        'item3_1': 'item3_1',
+        'item3_2': 'item3_2',
+        'item3_3': 'item3_3',
+        'item3_4': 'item3_4',
+        'item3_5': 'item3_5',
+        'item3_6': 'item3_6',
+        'item3_7': 'item3_7',
+        'item3_8': 'item3_8',
 
     }
 
