be2c8dfa3b0c9fe464cf702009503cb0acf1d8ef
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-12 01:39:51 +0300

    Second become first

--- my_lotos/main/data_base.py
+++ my_lotos/main/data_base.py
@@ -1,4 +1,4 @@
-
+import copy
 
 
 
@@ -14,9 +14,24 @@ item = {1: ['Здоровье', 'item1_1', 'item1_2', 'item1_3', 'item1_4', 'ite
         }
 
 
+item_copy = copy.deepcopy(item)
+for i in item:
+    for j in range(9):
+        item_copy[i][j] = 'text'
+
+item_copy[5] = item[3]
+item_copy[3][0] = item_copy[5][1]
+item_copy[6][0] = item_copy[5][2]
+item_copy[9][0] = item_copy[5][3]
+item_copy[8][0] = item_copy[5][4]
+item_copy[7][0] = item_copy[5][5]
+item_copy[4][0] = item_copy[5][6]
+item_copy[1][0] = item_copy[5][7]
+item_copy[2][0] = item_copy[5][8]
 
 
 
+item = copy.deepcopy(item_copy)
 
 
 data = {
@@ -34,81 +49,81 @@ data = {
     'item2_1': item[2][1],
     'item2_2': item[2][2],
     'item2_3': item[2][3],
-    'item2_4': item[1][4],
-    'item2_5': item[1][5],
-    'item2_6': item[1][6],
-    'item2_7': item[1][7],
-    'item2_8': item[1][8],
+    'item2_4': item[2][4],
+    'item2_5': item[2][5],
+    'item2_6': item[2][6],
+    'item2_7': item[2][7],
+    'item2_8': item[2][8],
 
-    'item3_0': item[2][0],
-    'item3_1': item[2][0],
-    'item3_2': item[20],
-    'item3_3': item[21],
-    'item3_4': item[22],
-    'item3_5': item[23],
-    'item3_6': item[24],
-    'item3_7': item[25],
-    'item3_8': item[26],
+    'item3_0': item[3][0],
+    'item3_1': item[3][1],
+    'item3_2': item[3][2],
+    'item3_3': item[3][3],
+    'item3_4': item[3][4],
+    'item3_5': item[3][5],
+    'item3_6': item[3][6],
+    'item3_7': item[3][7],
+    'item3_8': item[3][8],
 
-    'item4_0': item[27],
-    'item4_1': item[28],
-    'item4_2': item[29],
-    'item4_3': item[30],
-    'item4_4': item[31],
-    'item4_5': item[32],
-    'item4_6': item[33],
-    'item4_7': item[34],
-    'item4_8': item[35],
+    'item4_0': item[4][0],
+    'item4_1': item[4][1],
+    'item4_2': item[4][2],
+    'item4_3': item[4][3],
+    'item4_4': item[4][4],
+    'item4_5': item[4][5],
+    'item4_6': item[4][6],
+    'item4_7': item[4][7],
+    'item4_8': item[4][8],
 
-    'item5_0': item[36],
-    'item5_1': item[37],
-    'item5_2': item[38],
-    'item5_3': item[39],
-    'item5_4': item[40],
-    'item5_5': item[41],
-    'item5_6': item[42],
-    'item5_7': item[43],
-    'item5_8': item[44],
+    'item5_0': item[5][0],
+    'item5_1': item[5][1],
+    'item5_2': item[5][2],
+    'item5_3': item[5][3],
+    'item5_4': item[5][4],
+    'item5_5': item[5][5],
+    'item5_6': item[5][6],
+    'item5_7': item[5][7],
+    'item5_8': item[5][8],
 
-    'item6_0': item[45],
-    'item6_1': item[46],
-    'item6_2': item[47],
-    'item6_3': item[48],
-    'item6_4': item[49],
-    'item6_5': item[50],
-    'item6_6': item[51],
-    'item6_7': item[52],
-    'item6_8': item[53],
+    'item6_0': item[6][0],
+    'item6_1': item[6][1],
+    'item6_2': item[6][2],
+    'item6_3': item[6][3],
+    'item6_4': item[6][4],
+    'item6_5': item[6][5],
+    'item6_6': item[6][6],
+    'item6_7': item[6][7],
+    'item6_8': item[6][8],
 
-    'item7_0': item[54],
-    'item7_1': item[55],
-    'item7_2': item[56],
-    'item7_3': item[57],
-    'item7_4': item[58],
-    'item7_5': item[59],
-    'item7_6': item[60],
-    'item7_7': item[61],
-    'item7_8': item[62],
+    'item7_0': item[7][0],
+    'item7_1': item[7][1],
+    'item7_2': item[7][2],
+    'item7_3': item[7][3],
+    'item7_4': item[7][4],
+    'item7_5': item[7][5],
+    'item7_6': item[7][6],
+    'item7_7': item[7][7],
+    'item7_8': item[7][8],
 
-    'item8_0': item[63],
-    'item8_1': item[64],
-    'item8_2': item[65],
-    'item8_3': item[66],
-    'item8_4': item[67],
-    'item8_5': item[68],
-    'item8_6': item[69],
-    'item8_7': item[70],
-    'item8_8': item[71],
+    'item8_0': item[8][0],
+    'item8_1': item[8][1],
+    'item8_2': item[8][2],
+    'item8_3': item[8][3],
+    'item8_4': item[8][4],
+    'item8_5': item[8][5],
+    'item8_6': item[8][6],
+    'item8_7': item[8][7],
+    'item8_8': item[8][8],
 
-    'item9_0': item[72],
-    'item9_1': item[73],
-    'item9_2': item[74],
-    'item9_3': item[75],
-    'item9_4': item[76],
-    'item9_5': item[77],
-    'item9_6': item[78],
-    'item9_7': item[79],
-    'item9_8': item[80],
+    'item9_0': item[9][0],
+    'item9_1': item[9][1],
+    'item9_2': item[9][2],
+    'item9_3': item[9][3],
+    'item9_4': item[9][4],
+    'item9_5': item[9][5],
+    'item9_6': item[9][6],
+    'item9_7': item[9][7],
+    'item9_8': item[9][8],
 }
 
 
--- my_lotos/main/playground.py
+++ my_lotos/main/playground.py
@@ -1,12 +1,38 @@
-item = {0: ['Здоровье', 'item1_1', 'item1_2', 'item1_3', 'item1_4', 'item1_5', 'item1_6', 'item1_7', 'item1_8',],
-        1: ['Достависта', 'item2_1', 'item2_2', 'item2_3', 'item2_4', 'item2_5', 'item2_6', 'item2_7', 'item2_8',],
-        2: ['Профи', 'Поиск заказов', 'Переписка', 'Продажа', 'Ремонт', 'Инструмент', 'Запчасти', 'item3_7', 'item3_8',],
-        3: ['Питание', 'item4_1', 'item4_2', 'item4_3', 'item4_4', 'item4_5', 'item4_6', 'item4_7', 'item4_8',],
-        4: ['Москва', 'Профи', 'Сарафан', 'Кодинг', 'Обучение', 'Планирование', 'Питание', 'Здоровье', 'Достависта',],
-        5: ['Сарафан', 'item6_1', 'item6_2', 'item6_3', 'item6_4', 'item6_5', 'item6_6', 'item6_7', 'item6_8',],
-        6: ['Планирование', 'item7_1', 'item7_2', 'item7_3', 'item7_4', 'item7_5', 'item7_6', 'item7_7', 'item7_8',],
-        7: ['Обучение', 'item8_1', 'item8_2', 'item8_3', 'item8_4', 'item8_5', 'item8_6', 'item8_7', 'item8_8',],
-        8: ['Кодинг', 'item9_1', 'item9_2', 'item9_3', 'item9_4', 'item9_5', 'item9_6', 'item9_7', 'item9_8',],
+import copy
+
+
+item = {1: ['Здоровье', 'item1_1', 'item1_2', 'item1_3', 'item1_4', 'item1_5', 'item1_6', 'item1_7', 'item1_8',],
+        2: ['Достависта', 'item2_1', 'item2_2', 'item2_3', 'item2_4', 'item2_5', 'item2_6', 'item2_7', 'item2_8',],
+        3: ['Профи', 'Поиск заказов', 'Переписка', 'Продажа', 'Ремонт', 'Инструмент', 'Запчасти', 'item3_7', 'item3_8',],
+        4: ['Питание', 'item4_1', 'item4_2', 'item4_3', 'item4_4', 'item4_5', 'item4_6', 'item4_7', 'item4_8',],
+        5: ['Москва', 'Профи', 'Сарафан', 'Кодинг', 'Обучение', 'Планирование', 'Питание', 'Здоровье', 'Достависта',],
+        6: ['Сарафан', 'item6_1', 'item6_2', 'item6_3', 'item6_4', 'item6_5', 'item6_6', 'item6_7', 'item6_8',],
+        7: ['Планирование', 'item7_1', 'item7_2', 'item7_3', 'item7_4', 'item7_5', 'item7_6', 'item7_7', 'item7_8',],
+        8: ['Обучение', 'item8_1', 'item8_2', 'item8_3', 'item8_4', 'item8_5', 'item8_6', 'item8_7', 'item8_8',],
+        9: ['Кодинг', 'item9_1', 'item9_2', 'item9_3', 'item9_4', 'item9_5', 'item9_6', 'item9_7', 'item9_8',],
         }
 
-print(item[0][0])
+
+item_copy = copy.deepcopy(item)
+for i in item:
+    for j in range(9):
+        item_copy[i][j] = 'text'
+
+item_copy[5] = item[3]
+
+item_copy[3][0] = item_copy[5][1]
+item_copy[6][0] = item_copy[5][2]
+item_copy[9][0] = item_copy[5][3]
+item_copy[8][0] = item_copy[5][4]
+item_copy[7][0] = item_copy[5][5]
+item_copy[4][0] = item_copy[5][6]
+item_copy[1][0] = item_copy[5][7]
+item_copy[2][0] = item_copy[5][8]
+
+
+
+for i in item_copy:
+    print(item_copy[i])
+
+
+
